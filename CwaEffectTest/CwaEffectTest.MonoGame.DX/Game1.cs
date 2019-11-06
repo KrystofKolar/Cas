d254956b6f455b1aa09ccba503546197dcf52693
KrystofKolar <44490428+KrystofKolar@users.noreply.github.com>
2019-11-06 19:38:15 +0100

    base

---
+++ CwaEffectTest/CwaEffectTest.MonoGame.DX/Game1.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CwaEffectTest.MonoGame.DX
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        SpriteFont font;
+        Vector2 measure;
+        string text;
+        Vector2 c;
+
+        public Game1()
+        {
+            Window.AllowUserResizing = true;
+
+            graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = false;
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 480;
+            graphics.ApplyChanges();
+
+            Content.RootDirectory = "Content";
+
+            text = "CwaEffects.MonoGame.DX";
+        }
+
+        private void UpdateCenter()
+        {
+            c = new Vector2(graphics.PreferredBackBufferWidth / 2, graphics.PreferredBackBufferHeight / 2);
+            c.X -= measure.X / 2;
+            c.Y -= measure.Y / 2;
+        }
+
+        private void GraphicsDevice_DeviceReset(object sender, System.EventArgs e)
+        {
+            UpdateCenter();
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            GraphicsDevice gd = graphics.GraphicsDevice;
+            PresentationParameters pp = graphics.GraphicsDevice.PresentationParameters;
+            graphics.GraphicsDevice.DeviceReset += GraphicsDevice_DeviceReset;
+
+
+
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            font = Content.Load<SpriteFont>("font");
+            measure = font.MeasureString(text);
+
+            UpdateCenter();
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.DrawString(font, text, c, Color.White);
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
