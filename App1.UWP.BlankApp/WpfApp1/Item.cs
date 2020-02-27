c4527c1612da65176ad0fa3064bdc63253b7a087
KrystofKolar <44490428+KrystofKolar@users.noreply.github.com>
2020-02-27 21:08:52 +0100

    _

---
+++ App1.UWP.BlankApp/WpfApp1/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GfxItem
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ App1.UWP.BlankApp/WpfApp1/ItemViewable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GfxItem
+{
+    public class ItemViewable : Item
+    {
+        public bool Visible { get; set; }
+
+    }
+}
