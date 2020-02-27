64c5220396f7b38d445a737a5237f30dd3dd2934
KrystofKolar <44490428+KrystofKolar@users.noreply.github.com>
2020-02-27 10:10:43 +0100

    test

---
+++ App1.UWP.BlankApp/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //tbText.Text = "New text";
+            timer.Interval = new TimeSpan(10 * 1000 * 1000);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timer.Stop();
+
+            Hide();
+            Load lw = new Load();
+            lw.ShowDialog();
+            Close();
+        }
+
+        DispatcherTimer timer = new DispatcherTimer();
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+    }
+}
