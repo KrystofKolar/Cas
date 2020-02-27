0656106431bc0416d18fb4d48c8e7cda035f2e8a
KrystofKolar <44490428+KrystofKolar@users.noreply.github.com>
2020-02-27 15:24:15 +0100

    image size, converter

---
+++ App1.UWP.BlankApp/WpfApp1/ConverterWindowSize.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace CwaConverter
+{
+    [ValueConversion(typeof(double), typeof(double))]
+    public class ConverterWindowSize : IValueConverter
+    {
+        // Get int from int
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double r = Double.Parse(parameter as string, CultureInfo.InvariantCulture);
+            double result = (double)value * r;
+#if DEBUG
+            Debug.WriteLine($"Converted: {value} to {result}");
+#endif
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
