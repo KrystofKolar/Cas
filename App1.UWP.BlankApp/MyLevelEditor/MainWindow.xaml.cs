8cbfa01a41d1d3dd1f0f581fad5d6d766c416726
KrystofKolar <44490428+KrystofKolar@users.noreply.github.com>
2020-02-29 15:47:32 +0100

    Monogame added

---
+++ App1.UWP.BlankApp/MyLevelEditor/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MyLevelEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
