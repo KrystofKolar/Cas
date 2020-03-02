e3f71d9be5a4fe88401156a7efb8910fc3e165bb
KrystofKolar <44490428+KrystofKolar@users.noreply.github.com>
2020-03-02 21:06:32 +0100

    AppDomain Begin

---
+++ App1.UWP.BlankApp/Mg.AppDomain.NETCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mg.AppDomain.NETCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
